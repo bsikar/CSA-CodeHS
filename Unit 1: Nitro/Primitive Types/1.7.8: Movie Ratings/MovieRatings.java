9ffd09333bd66a8f558142e64dfd4d4f061ec4a9
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-29 23:32:29 -0600

    Create MovieRatings.java

---
+++ Unit 1: Nitro/Primitive Types/1.7.8: Movie Ratings/MovieRatings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MovieRatings
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+       Scanner input = new Scanner(System.in);
+       System.out.println((int) (.5 + input.nextDouble()));
+    }
+}
