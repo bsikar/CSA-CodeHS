e60ea3ae1444615dc423b50293ee43120a998a41
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-29 23:32:00 -0600

    Create CastingToDouble.java

---
+++ Unit 1: Nitro/Primitive Types/1.7.5: Casting to a Double/CastingToDouble.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CastingToDouble
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Scanner input = new Scanner(System.in);
+        int int1 = input.nextInt();
+        int int2 = input.nextInt();
+        System.out.println((double) int1 / (double) int2);
+    }
+}
