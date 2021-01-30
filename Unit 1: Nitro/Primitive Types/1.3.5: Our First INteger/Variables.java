c600062336806a9d1f52f77a382b5469fa5c355a
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-29 23:25:06 -0600

    Create Variables.java

---
+++ Unit 1: Nitro/Primitive Types/1.3.5: Our First INteger/Variables.java
@@ -0,0 +1,9 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        String a = "The current year is 2021";
+        System.out.println(a);
+    }
+}
