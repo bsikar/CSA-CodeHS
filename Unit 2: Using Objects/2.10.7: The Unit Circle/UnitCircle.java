bf3189257ee33e8368920d08f74787abcee77050
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 16:13:55 -0600

    Create UnitCircle.java

---
+++ Unit 2: Using Objects/2.10.7: The Unit Circle/UnitCircle.java
@@ -0,0 +1,13 @@
+public class UnitCircle 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Radians: (cos, sin)");
+        
+        // Put your code here!
+        System.out.println((double)0 + ": " + Math.round(Math.cos(0) * 100) / 100.0 + ", " + Math.round(Math.sin(0) * 100) / 100.0);
+        System.out.println(Math.PI / 2 + ": " + Math.round(Math.cos(Math.PI / 2) * 100) / 100.0 + ", " + Math.round(Math.sin(Math.PI / 2) * 100) / 100.0);
+        System.out.println(Math.PI + ": " + Math.round(Math.cos(Math.PI) * 100) / 100.0 + ", " + Math.round(Math.sin(Math.PI) * 100) / 100.0);
+
+    }
+}
