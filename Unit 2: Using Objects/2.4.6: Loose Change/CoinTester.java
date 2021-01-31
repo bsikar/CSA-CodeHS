f81fbe765c43970ae70b37b2ec2d6882895a06f7
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 15:58:13 -0600

    Create CoinTester.java

---
+++ Unit 2: Using Objects/2.4.6: Loose Change/CoinTester.java
@@ -0,0 +1,16 @@
+public class CoinTester
+{
+    public static void main(String[] args) {
+
+       //Create your program here
+       Coins coins = new Coins(4, 3, 2, 1); // 1.41
+       coins.bankValue();
+       coins.addPenny(); // 1.42, 11 coins
+       coins.addQuarter(); // 1.67 12 coins
+       coins.addQuarter(); // 1.92 13 coins
+       coins.addDime(); // 2.02 14 coins
+       coins.addDime(); // 2.12 15 coins
+       coins.bankCount();
+       coins.bankValue();
+    }
+}
