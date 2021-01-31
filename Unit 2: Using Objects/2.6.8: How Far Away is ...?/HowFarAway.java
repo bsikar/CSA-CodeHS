82c30a18a7f3b970cb658bf5a688c68d8ce37767
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 16:05:20 -0600

    Create HowFarAway.java

---
+++ Unit 2: Using Objects/2.6.8: How Far Away is ...?/HowFarAway.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HowFarAway 
+{
+    public static void main(String[] args)
+    {
+        // your code here.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the latitude of the starting location: ");
+        Double latStart = input.nextDouble();
+        System.out.print("Enter the longitude of the starting location: ");
+        Double lonStart = input.nextDouble();
+        GeoLocation start = new GeoLocation(latStart, lonStart);
+
+        System.out.print("Enter the latitude of the ending location: ");
+        Double latStop = input.nextDouble();
+        System.out.print("Enter the longitude of the ending location: ");
+        Double lonStop = input.nextDouble();
+        GeoLocation stop = new GeoLocation(latStop, lonStop);
+        System.out.print("The distance is " + start.distanceFrom(stop) + " miles.");
+    }
+}
