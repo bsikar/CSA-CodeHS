071388990e356d965388bc394bb492606be86368
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 16:07:32 -0600

    Create QuoteMachine.java

---
+++ Unit 2: Using Objects/2.7.9: QuoteMachine/QuoteMachine.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class QuoteMachine
+{
+    
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        // Ask for a quote
+        // Ask for the author
+        System.out.println("Enter a quote: ");
+        // Create a new String that has the quote in quotation marks
+        // Don't forget to escape the quotation marks
+        String quote = input.nextLine();
+        System.out.println("Enter the author of the quote: ");
+        String author = input.nextLine();
+        // Print the quote, then the author on the next line 
+        // But you can only use ONE print statement!
+        System.out.println("\"" + quote + "\"\n" + author);
+    }
+}
