6bfcca40d5dd5eb1a559ee3cf8272c2f206066f9
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 16:00:18 -0600

    Create SalutationsTester.java

---
+++ Unit 2: Using Objects/2.4.8: Greetings and Salutations/SalutationsTester.java
@@ -0,0 +1,8 @@
+public class SalutationsTester
+{
+    public static void main(String[] args)
+    {
+        // Test your class here
+        // System.out.println("Nothing is needed to make the code work / the tests to past ")");
+    }
+}
