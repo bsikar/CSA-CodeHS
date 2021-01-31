1364767e371b971d45b6ea39716b376cb032596d
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 15:58:45 -0600

    Create BotTester.java

---
+++ Unit 2: Using Objects/2.4.6: Loose Change/BotTester.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BotTester
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Put your code here
+        System.out.print("Hello. What is your name? What's the weather like?");
+        Bot bot = new Bot(input.nextLine());
+        bot.greeting();
+        bot.help();
+        bot.weather();
+        System.out.println("How many feet in a mile?");
+        bot.feetInMile();
+        bot.goodbye();
+    }
+}
