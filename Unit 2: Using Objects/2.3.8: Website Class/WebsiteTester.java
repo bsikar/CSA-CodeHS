614f4b89ee85c30acebb3e21b5c3ec6b17f4b7e3
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 15:54:28 -0600

    Create WebsiteTester.java

---
+++ Unit 2: Using Objects/2.3.8: Website Class/WebsiteTester.java
@@ -0,0 +1,14 @@
+public class WebsiteTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Website web1 = new Website();
+        Website web2 = new Website();
+        Website web3 = new Website();
+        System.out.println(web1);
+        System.out.println(web2);
+        System.out.println(web3);
+
+    }
+}
