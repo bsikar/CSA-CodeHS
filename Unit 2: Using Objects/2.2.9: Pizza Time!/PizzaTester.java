b1781840b039e6a8830aec8646237fc07cd24a77
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 15:51:48 -0600

    Create PizzaTester.java

---
+++ Unit 2: Using Objects/2.2.9: Pizza Time!/PizzaTester.java
@@ -0,0 +1,14 @@
+public class PizzaTester 
+{
+    public static void main(String[] args)
+    {
+        // Test your Pizza class here.
+        Pizza zah1 = new Pizza("Veggie", "Tomatoes, onions, olives", 12);
+        Pizza zah2 = new Pizza("Cheese", "Cheese", 15);
+        Pizza zah3 = new Pizza("Meat", "Pepperoni, sausage, bacon", 20);
+
+        System.out.println(zah1);
+        System.out.println(zah2);
+        System.out.println(zah3);
+    }
+}
