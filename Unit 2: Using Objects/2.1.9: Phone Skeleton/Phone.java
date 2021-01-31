dbab11728f65f3474202157898519b9e2da60c06
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 15:49:17 -0600

    Create Phone.java

---
+++ Unit 2: Using Objects/2.1.9: Phone Skeleton/Phone.java
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * The phone class represents a cellular phone.
+ * 
+ * Add the instance variables you think a phone would need
+ */ 
+
+public class Phone
+{
+
+    // Attributes
+    String plan;
+    Vector<String> apps;
+    float width;
+    float height;
+    
+
+}
