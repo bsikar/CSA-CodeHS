69c064b681ce343f3c8bb344f0c8eda59e06c061
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 23:01:43 -0600

    Create Numbers.java

---
+++ Unit 3: Boolean Expressions and If Statements/3.4.6: Positive, Negative, or Zero/Numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Numbers
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = input.nextInt();
+
+        if (n > 0) {
+            System.out.println("The number is positive!");
+        } else if (n == 0) {
+            System.out.println("The number is neither positive nor negative!");
+        } else {
+            System.out.println("The number is negative!");
+        }
+    }
+
+}
