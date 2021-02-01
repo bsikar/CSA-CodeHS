1295bba03683938af2c09a6ec87866ae39c28d4e
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 23:07:17 -0600

    Create ThreeStrings.java

---
+++ Unit 3: Boolean Expressions and If Statements/3.7.9: Three Strings/ThreeStrings.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ThreeStrings
+{
+    public static void main(String[] args)
+    {
+        // Ask the user for three strings.
+        // Use a Boolean variable to test the comparison of 
+        // first+second equals third
+        // Remember since you are working with strings to 
+        // use equals() and NOT == !
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("First string? ");
+        String a = input.nextLine();
+        System.out.print("Second string? ");
+        String b = input.nextLine();
+        System.out.print("Third string? ");
+        String c = input.nextLine();
+        
+        if (a.concat(b).equals(c)) {
+            System.out.println(a + " + " + b + " is equal to " + c + "!");
+        } else {
+            System.out.println(a + " + " + b + " is not equal to " + c + "!");
+        }
+    }
+}
