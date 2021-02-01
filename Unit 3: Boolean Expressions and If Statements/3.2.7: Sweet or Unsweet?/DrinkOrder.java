e573e9ef4a218efca64fd1f5040b2d93df1744b5
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-31 22:55:56 -0600

    Create DrinkOrder.java

---
+++ Unit 3: Boolean Expressions and If Statements/3.2.7: Sweet or Unsweet?/DrinkOrder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DrinkOrder
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        // Start here!
+        // You'll find it helpful to list the steps you
+        // need to take, then write the code
+        System.out.println("What do you want to drink? ");
+        String drink = input.nextLine();
+        System.out.println("How many teaspoons of sugar do you want? ");
+        int sugar = input.nextInt();
+        
+        System.out.println("Confirming your order. You wanted: \n" + drink + ((sugar > 0) ? " with sugar" : ""));
+    }
+}
